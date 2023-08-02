5f799eaf206093bcbf5db28507884f0e10dfed01
RAHUL RAMAN <58595285+rahul7070@users.noreply.github.com>
2023-08-02 06:33:44 +0530

    df

--- my-react-app/src/services/api.js
+++ my-react-app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = ""
+const API_URL = "https://gmailapp-9pdo.onrender.com"
 
 const API_GMAIL = async (urlObject, payload, type)=>{
     return await axios({
